466d049495b973bc2305510e19b583e04480e0ff
venvxiangni <minsbayer@gmail.com>
2025-11-16 21:42:09 +0700

    feature: Add Update Profile and Change Pass

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,7 @@ import { verifyToken } from "../middleware/authJWT.js";
 import UserModel from "../models/user.model.js";
 
 
+// Get information of a user
 export const GetProfileInfo = async (req,res) => {
     try{
 
@@ -16,12 +17,77 @@ export const GetProfileInfo = async (req,res) => {
         })
 
         if(!userInfor){
-            res.status(401).send({message: "User not found"})
+            return res.status(401).send({message: "User not found"})
         }
 
-        res.status(200).send(userInfor)
+        return res.status(200).send(userInfor)
 
     }catch(error){
-        res.status(500).send({message: error.message})
+        return res.status(500).send({message: error.message})
+    }
+}
+
+
+// Update profile
+export const UpdateProfileInfo = async (req,res) => {
+
+    try{
+
+        const userId = req.userId;
+
+        const {fullName, email} = req.body;
+    
+        if (!fullName || !email){
+            res.status(400).send("Missing information");
+        }
+        
+        const userInfor = await UserModel.findOne({where: {id: userId}, attributes: {exclude: ["password"]}});
+
+        if(!userInfor){
+            return res.status(404).send("Not found User")
+        }
+
+        userInfor.fullName = fullName;
+        userInfor.email = email;
+
+        await userInfor.save()
+        
+
+        return res.status(200).send({message: "Updated Successfully", user: {userInfor}})
+
+    }catch(error){
+        return res.status(500).send({message: error.message})
+    }
+
+}
+
+export const ChangePassword = async(req,res) => {
+    try{
+
+        const userId = req.userId;
+
+        const {currentPassword,newPassword} = req.body
+
+        const userInfo = await UserModel.findOne({where:{id: userId}})
+
+        if(!userInfo){
+            return res.status(404).send("User not found");
+        }
+
+        //check password
+        if(currentPassword !== userInfo.password){
+            return res.status(403).send("Password not match");
+        }
+
+        userInfo.password = newPassword;
+
+        await userInfo.save()
+
+        return res.status(200).send("Update password success")
+
+        
+
+    }catch(error){
+        return res.status(500).send({message: error.message})
     }
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,14 @@
-import { GetProfileInfo } from "../controllers/user.controller.js";
+import { GetProfileInfo,UpdateProfileInfo,ChangePassword } from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/authJWT.js";
 
 
 
 const authUser = (app) => {
    
-    app.get('/api/user/me',verifyToken,GetProfileInfo)
+    app.get('/api/user/profile',verifyToken,GetProfileInfo);
+    app.post('/api/user/profile',verifyToken,UpdateProfileInfo);
+    app.post('/api/user/changepassword',verifyToken,ChangePassword)
 }
+
 
 export default authUser;
